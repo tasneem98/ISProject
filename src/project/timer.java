7bd50265b3e79d6471f1bb742be1aeaedcc092e8
Tasneem Madeh <tasneemmadeh98@gmail.com>
2020-10-11 08:45:29 +0200

    IS Project

---
+++ src/project/timer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author الدولية
+ */
+class timer {
+
+    static Object getTimer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
